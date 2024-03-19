75434b00d93d07e028b2882be05e971c0a8f589b
KunKimlong <kimlongkun2412@gmail.com>
2024-03-20 01:51:19 +0700

    update

---
+++ assets/Js/data.js
@@ -0,0 +1,100 @@
+
+const skill = [
+    {
+        based:'Spoken language',
+        part:[
+            {
+                language:'Khmer',
+                percent:100
+            },
+            {
+                language:'English',
+                percent:65
+            }
+        ]
+    },
+    {
+        based:'Basic programming',
+        part:[
+            {
+                language:'C programming',
+                percent:90
+            },
+            {
+                language:'C++ pogramming',
+                percent:85
+            }
+        ]
+    },
+    {
+        based:'Frontend development',
+        part:[
+            {
+                language:'HTML',
+                percent:100
+            },
+            {
+                language:'CSS',
+                percent:80
+            },
+            {
+                language:'Bootstrap',
+                percent:75
+            },
+            {
+                language:'Javascript',
+                percent:60
+            },
+            {
+                language:'Jquery',
+                percent:60
+            },
+            {
+                language:'React Js/ts',
+                percent:20
+            },
+            {
+                language:'Vue Js/ts',
+                percent:40
+            },
+            {
+                language:'Ajax',
+                percent:50
+            },
+        ]
+    },
+    {
+        based:'Backend development',
+        part:[
+            {
+                language:'PHP',
+                percent:85
+            },
+            {
+                language:'Laravel',
+                percent:60
+            },
+            {
+                language:'C#',
+                percent:50
+            },
+            {
+                language:'Java',
+                percent:20
+            },
+        ]
+    },
+    {
+        based:'Version controll',
+        part:[
+            {
+                language:'git',
+                percent:40
+            },
+            {
+                language:'github/gitlab',
+                percent:50
+            },
+        ]
+    },
+]
--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
-    // $(window).on('load',function(){
-        // alert(123)
+    
     var heightNavbar = $('.navbar').outerHeight();
     // alert(heightNavbar)
     $('.blog-about').css('height','calc(100vh - '+heightNavbar+'px)');
     $('.blog-about').css('margin-top',heightNavbar+'px');
-    // })
+    
+    console.log(skill);
+    var txt = '';
+    skill.forEach(el=>{
+        txt +=`
+            <li>
+                  + ${el.based}
+                  <ul>`;
+        var parts = el.part;
+        parts.forEach(part=>{
+            txt+=`
+                <li>
+                    <div class="title">${part.language}</div>
+                    <div class="outside-gray">
+                    <div class="inside-gray" style="width:${part.percent}%"></div>
+                    </div>
+                </li>
+            `
+        })
+        txt+=`
+            </ul>
+        </li>
+        `;
+    })
+
+    $('#skill').html(txt)
 
     $('body').on('click','.bi-justify-left',function(){
         $('.navbar-collapse').css('top','60px');
